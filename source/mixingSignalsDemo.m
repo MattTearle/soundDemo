87106b30efc3cf2c92c3e78be335680645c20aa9
unknown <mtearle@AH-MTEARLE.dhcp.mathworks.com>
2017-03-06 15:08:47 -0500

    Cleanup, added comments, no need to display app info to command window

--- source/mixingSignalsDemo.m
+++ source/mixingSignalsDemo.m
@@ -1,23 +1,27 @@
 classdef mixingSignalsDemo < handle
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
+    % App to demonstrate combining sine waves of different frequencies and
+    % amplitudes
     
-    properties
-        fig
-        ax
-        plot
-        freq
-        freqtxt
-        amp
-        playbutt
-        t
-        twopit
-        y
-        nplotpts
+    properties (Access = private)
+        % interface objects
+        fig         % figure window
+        ax          % array of axes for sound wave plots
+        plot        % array of plots (line objects) for sound waves
+        freq        % array of sliders for individual input frequencies
+        freqtxt     % array of text objects to show slider values
+        amp         % array of sliders for individual input amplitudes
+        playbutt    % play button
+        % app data
+        t           % time vector for sound waves
+        twopit      % 2*pi*t
+        y           % matrix holding y(t) for three individual waves + total
+        nplotpts    % number of points to plot (subset of total wave)
     end
     
     methods
+        % constructor
         function app = mixingSignalsDemo
+            % get screen geometry & determine app position
             r = groot;
             ws = r.ScreenSize;
             h = ws(4) - ws(2);
@@ -25,9 +29,12 @@ classdef mixingSignalsDemo < handle
             xoff = round(0.1*(ws(3)-ws(1)));
             h = round(0.8*h);
             w = h;
+            % build figure window
             app.fig = figure('Position',[xoff yoff w h],...
                 'MenuBar','none','Toolbar','none',...
+                'IntegerHandle','off','HandleVisibility','callback',...
                 'Name','Signal Mixer','NumberTitle','off');
+            % axes
             app.ax = gobjects(4,1);
             for k = 1:3
                 app.ax(k) = axes('Parent',app.fig,...
@@ -35,6 +42,7 @@ classdef mixingSignalsDemo < handle
             end
             app.ax(4) = axes('Parent',app.fig,...
                 'Units','normalized','Position',[0.1 0.05 0.6 0.3]);
+            % frequency sliders
             app.freq = gobjects(3,1);
             vals = [440 220 880];
             for k = 1:3
@@ -42,6 +50,7 @@ classdef mixingSignalsDemo < handle
                     'Units','normalized','Position',[0.75 (1.1-0.2*k) 0.15 0.02],...
                     'Min',200,'Max',1000,'Value',vals(k),'Callback',@app.updateplot);
             end
+            % slider text
             app.freqtxt = gobjects(3,1);
             for k = 1:3
                 app.freqtxt(k) = uicontrol(app.fig,'Style','text',...
@@ -54,6 +63,7 @@ classdef mixingSignalsDemo < handle
                     'String','Frequency [Hz]','FontSize',10,...
                     'HorizontalAlignment','center');
             end
+            % amplitude sliders
             app.amp = gobjects(3,1);
             for k = 1:3
                 app.amp(k) = uicontrol(app.fig,'Style','slider',...
@@ -62,13 +72,17 @@ classdef mixingSignalsDemo < handle
             end
             app.amp(1).Value = 1;
             
+            % play button
             icon = load('icons','spkrrun');
             app.playbutt = uicontrol(app.fig,'Style','pushbutton',...
                 'Units','normalized','Position',[0.8 0.05 0.1 0.05],...
                 'Callback',@app.play,'CData',icon.spkrrun);
+            % set (and call) callback to adjust button size/position when
+            % window is resized
             app.fig.SizeChangedFcn = @app.repositionplaybutton;
             repositionplaybutton(app,[],[])
             
+            % Make plots for individual waves
             app.nplotpts = 501;
             app.t = linspace(0,1,1 + (app.nplotpts-1)*50)';
             app.twopit = 2*pi*app.t;
@@ -76,41 +90,53 @@ classdef mixingSignalsDemo < handle
             for k = 1:3
                 app.y(:,k) = app.amp(k).Value*sin(app.twopit*app.freq(k).Value);
             end
+            % Total wave
             app.y(:,4) = sum(app.y(:,1:3),2);
-            
+            % Make plots
             app.plot = gobjects(4,1);
-%             n = 1 + 0.02*fs;
             for k = 1:4
-                app.plot(k) = plot(app.ax(k),app.t(1:app.nplotpts),app.y(1:app.nplotpts,k)); %#ok<CPROP>
+                app.plot(k) = plot(app.ax(k),app.t(1:app.nplotpts),app.y(1:app.nplotpts,k));
             end
-            
+            % Set axis limits
             for k = 1:3
                 app.ax(k).YLim = [-1 1];
             end
         end
         
+        % callback for any change to the sliders
         function updateplot(app,~,~)
+            % Update individual sound waves
             for k = 1:3
                 app.y(:,k) = app.amp(k).Value*sin(app.twopit*app.freq(k).Value);
                 app.plot(k).YData = app.y(1:app.nplotpts,k);
                 app.freqtxt(k).String = num2str(app.freq(k).Value);
             end
+            % Update total
             app.y(:,4) = sum(app.y(:,1:3),2);
             app.plot(4).YData = app.y(1:app.nplotpts,4);
         end
         
+        % callback for playing sound (play button pressed)
         function play(app,~,~)
+            % play total wave for 1 second
             soundsc(app.y(:,4),length(app.t)-1)
         end
         
+        % callback for adjusting size/pos of button when window is resized
         function repositionplaybutton(app,~,~)
+            % get position in pixels
             pb = app.playbutt;
             pb.Units = 'pixels';
             p = pb.Position;
+            % adjust position & reset units
             pb.Position = [p(1)+p(3)/2-26,p(2)+p(4)/2-26,52,52];
             pb.Units = 'normalized';
         end
+        
+        % no need to display anything about the app to the command window
+        function display(app) %#ok<MANU,DISPLAY>
+        end
+        
     end
     
 end
-
--- source/overtonesDemo.m
+++ source/overtonesDemo.m
@@ -1,24 +1,27 @@
 classdef overtonesDemo < handle
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
+    % App to demonstrate how overtones create different instrument sounds
     
-    properties
-        fig
-        ax
-        ottxt
-        playbutt
-        amp
-        keypatches
-        keymap
-        keynum
-        f0
-        freqRef
-        notenames
-        overtones
+    properties (Access = private)
+        % interface graphics objects
+        fig         % figure window
+        ax          % axes for keyboard
+        ottxt       % array of text objects to display overtone note names
+        playbutt    % sound play button
+        amp         % array of sliders for amplitude of each overtone
+        keypatches  % graphics patches for keys
+        % app data
+        keymap      % mapping of keys to patch objects
+        keynum      % mapping of patch objects to keys
+        f0          % fundamental frequency (note clicked by user)
+        overtones   % number of keys to shift for each overtone
+        freqRef     % list of reference frequencies for each key
+        notenames   % list of note names for each key
     end
     
     methods
+        % constructor
         function app = overtonesDemo
+            % get screen geometry & determine app position
             r = groot;
             ws = r.ScreenSize;
             w = ws(3) - ws(1);
@@ -26,19 +29,29 @@ classdef overtonesDemo < handle
             yoff = round(0.15*(ws(4)-ws(2)));
             w = round(0.75*w);
             h = round(0.5*w);
+            % build figure window
             app.fig = figure('Position',[xoff yoff w h],...
                 'Toolbar','none','MenuBar','none',...
+                'IntegerHandle','off',...
                 'NumberTitle','off','Name','Overtones Mixer');
             app.ax = axes('Parent',app.fig,...
                 'Units','normalized','Position',[0.05 0.5 0.9 0.4]);
             
+            % number of octaves in keyboard
             numocts = 7;
+            % build note names
+            % names in an octave
             notes = repmat({'C','C#','D','Eb','E','F','F#','G','G#','A','Bb','B'}',1,numocts);
+            % octaves
             octs = repmat(1+(1:numocts),12,1);
             octs = cellstr(num2str(octs(:)));
+            % join together to make individual note names
             app.notenames = strcat(notes(:),octs);
+            % make reference frequencies and overtone key shifts
             app.freqRef = 440*pow2((-34+(1:12*numocts))/12);
             app.overtones = round(12*log2([0.5 1:7]));
+            
+            % text objects for overtones (initially invisible)
             app.ottxt = gobjects(length(app.overtones),1);
             ot = ['1/2';'base';cellstr(num2str((2:7)'))];
             for k = 1:length(app.overtones)
@@ -48,6 +61,7 @@ classdef overtonesDemo < handle
                     'HorizontalAlignment','center','FontSize',10);
             end
             
+            % amplitude sliders
             app.amp = gobjects(8,1);
             for k = 1:8
                 r = mod(k-1,4)+1;
@@ -61,24 +75,38 @@ classdef overtonesDemo < handle
             end
             app.amp(2).Value = 1;
             
+            % play button
             icon = load('icons','spkrrun');
             app.playbutt = uicontrol(app.fig,'Style','pushbutton',...
                 'Units','normalized','Position',[0.075 0.25 0.1 0.1],...
                 'Callback',@app.play,'CData',icon.spkrrun);
+            % set (and call) callback to adjust button size/position when
+            % window is resized
             app.fig.SizeChangedFcn = @app.repositionplaybutton;
             repositionplaybutton(app,[],[])
             
+            % reset button (to reset sliders)
             uicontrol(app.fig,'Style','pushbutton',...
                 'Units','normalized','Position',[0.075 0.15 0.1 0.05],...
                 'Callback',@app.resetamps,'String','Reset');
             
+            % make keyboard
             [app.keypatches,app.keymap,app.keynum] = makekeyboard(12*numocts,app.ax);
+            % Make figure hidden to other plotting (otherwise plots go into
+            % app). Had to wait so makekeyboard had access to axes
+            
+            % add callback to keys on callback
             [app.keypatches.ButtonDownFcn] = deal(@app.keypressed);
+            % add callback to sliders
             [app.amp.Callback] = deal(@app.keypressed);
+            % initialize f0 (just so it exists)
             app.f0 = 0;
         end
         
+        % callback for any change to key or sliders
         function keypressed(app,key,~)
+            % Reset keyboard (black and white) -- keys are ordered black
+            % (5/12 of keys), then white (7/12)
             n = length(app.keypatches);
             for k = 1:(5*n/12)
                 app.keypatches(k).FaceColor = [0 0 0];
@@ -86,61 +114,94 @@ classdef overtonesDemo < handle
             for j = (k+1):n
                 app.keypatches(j).FaceColor = [1 1 1];
             end
+            % What was clicked? Patch => key
             if isequal(key.Type,'patch')
-            idx = app.keynum(app.keypatches == key);
-            app.ottxt(2).String = app.notenames(idx);
-            app.f0 = app.freqRef(idx);
+                % Find which key was pressed
+                idx = app.keynum(app.keypatches == key);
+                % Get the key name and frequency
+                app.ottxt(2).String = app.notenames(idx);
+                app.f0 = app.freqRef(idx);
             else
+                % Slider
+                % Make sure we have a valid frequency selected (you can
+                % change sliders before ever selecting a key and that's OK)
                 idx = find(app.f0 == app.freqRef);
                 if isempty(idx)
                     idx = NaN;
                 end
             end
             
+            % Get some window layout geometry to help position text
             p = app.ax.Position;
             scl = p(3)/diff(app.ax.XLim);
             
+            % Get the amplitude slider values
             A = [app.amp.Value];
+            % Loop over overtones
             for k = 1:length(app.overtones)
+                % Get key index
                 idx2 = idx + app.overtones(k);
+                % Label the key (assuming it's on the keyboard)
                 if (idx2>0) && (idx2<=n)
+                    % Get the key patch
                     j = app.keymap(idx2);
+                    % Position the text
                     app.ottxt(k).Position(1) = p(1) - 0.015 + scl*median(app.keypatches(j).XData);
                     app.ottxt(k).Visible = 'on';
+                    % Color the key according to amplitude
                     if (app.keypatches(j).FaceColor(1) == 1)
+                        % white key -> reduce G&B to make R
                         app.keypatches(j).FaceColor = [1 1-A(k) 1-A(k)];
                     else
+                        % black key -> increase R
                         app.keypatches(j).FaceColor = [A(k) 0 0];
                     end
                 else
+                    % Turn off text for any note not currently on keyboard
                     app.ottxt(k).Visible = 'off';
                 end
             end
         end
         
+        % callback to play sound
         function play(app,~,~)
+            % Make vector for time and y (initially 0)
             t = 2*pi*linspace(0,1,44100);
             f = [1/2 1:7];
             y = 0*t;
+            % Loop over overtones
             for k = 1:length(f)
                 y = y + app.amp(k).Value*sin(t*f(k)*app.f0);
             end
+            % Play the result
             soundsc(y,44100)
         end
         
+        % callback for adjusting size/pos of button when window is resized
         function repositionplaybutton(app,~,~)
+            % get position in pixels
             pb = app.playbutt;
             pb.Units = 'pixels';
             p = pb.Position;
+            % adjust position & reset units
             pb.Position = [p(1)+p(3)/2-26,p(2)+p(4)/2-26,52,52];
             pb.Units = 'normalized';
         end
         
+        % callback for resetting amplitude sliders
         function resetamps(app,obj,~)
+            % set all to zero
             [app.amp.Value] = deal(0);
+            % set f0 amplitude to 1
             app.amp(2).Value = 1;
+            % update keyboard
             keypressed(app,obj,[])
         end
+        
+        % no need to display anything about the app to the command window
+        function display(app) %#ok<MANU,DISPLAY>
+        end
+        
     end
     
 end
